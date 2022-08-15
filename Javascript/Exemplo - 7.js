77f9cb0555c23baeb396c06c7c7880b12b533fec
Yohana Mayra <yohanamayra@guide.com.br>
2022-08-15 13:26:08 -0300

    Exemplos em JS

--- Javascript/Exemplo - 7.js
+++ Javascript/Exemplo - 7.js
@@ -1,8 +1,9 @@
-var Nota1 =prompt("Digite sua primeira nota");
-var Nota2 =prompt("Digite sua segunda nota");
-var Nota3 =prompt("Digite sua terceira nota");
-var Nota4 =prompt("Digite sua quarta nota");
+var Nota1 = 2;
+var Nota2 = 6;
+var Nota3 = 10;
+var Nota4 = 5;
 
 var Soma =(parseFloat(Nota1)+parseFloat(Nota2)+parseFloat(Nota3)+parseFloat(Nota4));
 var Media = Soma / 4;
-    console.log("A sua média é de " + Soma);
+
+console.log("A sua média é de " + Soma);
