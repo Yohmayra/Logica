61bac48de0aa1f375f49530df09461c015f00065
Yohana Mayra <109821205+Yohmayra@users.noreply.github.com>
2022-08-16 13:47:43 -0300

    Exemplos em Java

--- Java/Exemplo1.java
+++ Java/Exemplo1.java
@@ -8,11 +8,9 @@ public class Exemplo1{
 public static void main (String[]args){
     total = (prod * qtd)*5;
         System.out.println(total);
-
     if (idade == 31) {
         System.out.println(nome);
     }
-    
     idade = 33;
     if (idade == 33) {
         nome += " Egidio";
